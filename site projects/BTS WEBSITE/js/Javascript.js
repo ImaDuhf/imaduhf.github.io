4ac1697c021f80e3992fcabf948b475bac0a4550
ImaDuhf <timperson234@gmail.com>
2021-12-11 18:10:19 +0100

    Changed album title texts to buttons to prepare adding information

---
+++ site projects/BTS WEBSITE/js/Javascript.js
@@ -0,0 +1,7 @@
+
+
+/* Det här ska in när man klickar på knappen och bort när man klickar på den igen eller en annan knapp
+<div class="col border border-dark border-2">
+    <p class="album-info-text mt-4 ms-2">2 Cool 4 Skool Album</p>
+</div>
+*/
