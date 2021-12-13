8aa711b8c17d154ffcb78465fb99f8b3e274df5b
ImaDuhf <timperson234@gmail.com>
2021-12-13 19:06:46 +0100

    buttons
    Didnt work out the way i liked so i just reset

--- site projects/BTS WEBSITE/js/Javascript.js
+++ site projects/BTS WEBSITE/js/Javascript.js
@@ -5,3 +5,33 @@
     <p class="album-info-text mt-4 ms-2">2 Cool 4 Skool Album</p>
 </div>
 */
+
+var skool = document.getElementById("2cool4skool")
+var oaul = document.getElementById("orul82")
+var skooldiv = document.getElementById("div2cool4skool")
+var oauldiv = document.getElementById("divorul82")
+
+
+function openInfo(num) {
+    if (num == 1) {
+        skool.hidden = false;
+        skooldiv.innerHTML = '<button class="album-title-text" onclick="openInfo(001)">2 Cool 4 Skool</button>'
+        console.log("1")
+    }
+    else if (num == 001) {
+        skool.hidden = true;
+        skooldiv.innerHTML = '<button class="album-title-text" onclick="openInfo(1)">2 Cool 4 Skool</button>'
+        console.log("001")
+    }
+
+    if (num == 2) {
+        oaul.hidden = false;
+        oauldiv.innerHTML = '<button class="album-title-text" onclick="openInfo(002)">O!RUL8,2?</button>'
+        console.log("2")
+    }
+    else if (num == 002) {
+        oaul.hidden = true;
+        oauldiv.innerHTML = '<button class="album-title-text" onclick="openInfo(2)">O!RUL8,2?</button>'
+        console.log("002")
+    }
+}
